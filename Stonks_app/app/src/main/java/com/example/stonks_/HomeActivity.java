cc56a8cf31640d909375057035cbac91080238b8
Amalhafiz786 <amalhafiz123@gmail.com>
2020-11-17 12:26:52 +0530

    test

---
+++ Stonks_app/app/src/main/java/com/example/stonks_/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.example.stonks_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        getSupportActionBar().hide();
+
+
+    }
+}
