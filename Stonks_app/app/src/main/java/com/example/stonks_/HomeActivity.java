ec01b957140c64c508b9a4ca6e15a7ca0d11457a
Gracious Baby <gracious932@gmail.com>
2020-11-21 21:48:39 +0530

    Updated Server conn(POST)

--- Stonks_app/app/src/main/java/com/example/stonks_/HomeActivity.java
+++ Stonks_app/app/src/main/java/com/example/stonks_/HomeActivity.java
@@ -12,8 +12,11 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.Request;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -38,25 +41,30 @@ public class HomeActivity extends AppCompatActivity {
                 RequestQueue queue = Volley.newRequestQueue(HomeActivity.this);
                 String url = "http://192.168.0.8:5000";
 
-// Request a string response from the provided URL.
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                setContentView(R.layout.result);
-                                Toast.makeText(getApplicationContext(),response,Toast.LENGTH_SHORT).show();
-                                Log.i("stonks", response);
-                            }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getApplicationContext(),error.getMessage(),Toast.LENGTH_SHORT).show();
-                        Log.i("stonks","fail" + error.getMessage());
-                    }
-                });
+                String json = "{\"query\":\""+ query +"\"}";
 
-// Add the request to the RequestQueue.
-                queue.add(stringRequest);
+                try {
+
+                    JSONObject obj = new JSONObject(json);
+
+                    Log.i("stonks", obj.toString());
+                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, obj, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Log.i("stonks",response.toString());
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.i("stonks",error.toString());
+                        }
+                    });
+                    queue.add(jsonObjectRequest);
+
+                } catch (Throwable tx) {
+                    Log.e("stonks", "Could not parse malformed JSON: \"" + json + "\"");
+                }
+
                 return true;
             }
 
